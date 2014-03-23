acc99391de66f237258f3e56abb73c3adaf9671e
st028 <sam.thompson028@utexas.edu>
2014-03-23 18:23:44 -0500

    blah

--- application/views/dash/point_reqs.php
+++ application/views/dash/point_reqs.php
@@ -1,25 +1,26 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Remaining points needed</h1>
           <div class="row placeholders">
-            <div class="col-xs-6 col-sm-6 placeholder text-center">
+            <div class="col-sm-6 placeholder text-center">
               <font size="7"><?=$points[EVENT]; ?>/<?=$pp['event']; ?></font>
               <h4>Event</h4>
               <span class="text-muted">Something else</span>
             </div>
-            <div class="col-xs-6 col-sm-6 placeholder text-center">
+            <div class="col-sm-6 placeholder text-center">
               <font size="7"><?=$points[MEETING]; ?>/<?=$pp['fun']; ?></font>
               <h4>Fundraising</h4>
               <span class="text-muted">Something else</span>
             </div>
-            <div class="col-xs-6 col-sm-6 placeholder text-center">
+            <div class="col-sm-6 placeholder text-center">
               <font size="7"><?=$points[FUNDRAISING]; ?>/<?=$pp['meeting']; ?></font>
               <h4>Meeting</h4>
               <span class="text-muted">Something else</span>
             </div>
-            <div class="col-xs-6 col-sm-6 placeholder text-center">
+            <div class="col-sm-6 placeholder text-center">
               <font size="7"><?=$points[SOCIAL]; ?>/<?=$pp['social']; ?></font>
               <h4>Social</h4>
               <span class="text-muted">Something else</span>
+            </div>
             </div>
           </div>
         </div>
