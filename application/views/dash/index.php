be8dcc777a6bd706cfc0901cf99ac5a86012ee76
st028 <sam.thompson028@utexas.edu>
2014-03-23 19:59:08 -0500

    removed page-headers for dash: redundant since sidebar tells you where you are

--- application/views/dash/index.php
+++ application/views/dash/index.php
@@ -1,5 +1,4 @@
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Announcements</h1>
           <div class="row">
               <div class="col-md-2"></div>
               <div class="col-md-8">
@@ -7,6 +6,26 @@
                 <div class="clock" style="padding-top:10px; padding-bottom: 10px"></div>
               </div>
               <div class="col-md-2"></div>
+          </div>
+          <div class="row">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+              <h3 class="panel-title">Test heading</h3>
+              </div>
+              <div class="panel-body">
+              Test announcement
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+              <h3 class="panel-title">Test heading</h3>
+              </div>
+              <div class="panel-body">
+              Test announcement
+              </div>
+            </div>
           </div>
           <div class="row">
             <div class="panel panel-info">
--- application/views/dash/point_reqs.php
+++ application/views/dash/point_reqs.php
@@ -1,7 +1,6 @@
 
  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 
-          <h1 class="page-header">My points</h1>
 
           <div class="row placeholders">
 
