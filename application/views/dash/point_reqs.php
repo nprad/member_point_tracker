9e0f2316dedeed7f4830f48445c2aca0a90065db
st028 <sam.thompson028@utexas.edu>
2014-03-23 19:15:28 -0500

    weird formatting pt 2

--- application/views/dash/point_reqs.php
+++ application/views/dash/point_reqs.php
@@ -1,4 +1,4 @@
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <!--<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Remaining points needed</h1>
           <div class="row placeholders">
             <div class="col-sm-6 placeholder text-center">
@@ -22,5 +22,50 @@
               <span class="text-muted">Something else</span>
             </div>
             </div>
+          </div>
+        </div> -->
+
+ <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+          <h1 class="page-header">My points</h1>
+
+          <div class="row placeholders">
+
+            <div class="col-md-6">
+              <div class="panel panel-default">
+                <font size="7" class="<?php echo $points[EVENT] < $pp['event'] ? '' : 'green'; ?>"><?=$points[EVENT]; ?>/<?=$pp['event']; ?></font>
+                <div class="panel-footer">
+                  <h4>Event</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="panel panel-default">
+                <font size="7" class="<?php echo $points[MEETING] < $pp['meeting'] ? '' : 'green'; ?>"><?=$points[MEETING]; ?>/<?=$pp['meeting']; ?></font>
+                <div class="panel-footer">
+                  <h4>Meeting</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="panel <?php echo $points[FUNDRAISING] < $pp['fun'] ? 'panel-default' : 'panel-success'; ?>">
+                <font size="7" class=""><?=$points[FUNDRAISING]; ?>/<?=$pp['fun']; ?></font>
+                <div class="panel-footer">
+                  <h4>Fundraising</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="panel <?php echo $points[SOCIAL] < $pp['social'] ? 'panel-default' : 'panel-success'; ?>">
+                <font size="7"><?=$points[SOCIAL]; ?>/<?=$pp['social']; ?></font>
+                <div class="panel-footer">
+                  <h4>Social</h4>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
